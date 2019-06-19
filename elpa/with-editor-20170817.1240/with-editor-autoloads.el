fefb036d996dcc8af467911aa1640188c292d611
Mitchell Bright <mitch@newie.ventures>
2019-06-19 14:06:43 +1000

    Untrack elpa folder.

--- elpa/with-editor-20170817.1240/with-editor-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; with-editor-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("with-editor-pkg.el" "with-editor.el")
-;;;;;;  (22983 14312 498298 319000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; with-editor-autoloads.el ends here
